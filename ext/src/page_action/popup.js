14bcf4057e0d923417c0eaf8eeb2332936f871b1
Joshua Ryan <shua2727@gmail.com>
2016-04-01 08:38:56 -0400

    Add untracked files

---
+++ ext/src/page_action/popup.js
@@ -0,0 +1,71 @@
+chrome.tabs.getSelected(null, function(tab) {
+    document.getElementById('currentLink').innerHTML = tab.title;
+
+  
+    console.log(tab);
+    var socket = io('https://lehighhackers.mybluemix.net/');
+    socket.emit('url', { url: tab.url });
+
+    socket.on('resp', function(data){
+        console.log("Data = %o", data);
+        if (data.status == "ERROR"){            
+            $('#mainPopup').text(data.statusInfo);
+            return;
+        }
+        
+        
+        var author = data.author;
+        var concepts = data.concepts;
+        var entities = data.entities;
+
+        var body = document.body;
+        tbl  = document.createElement('table');
+        $(tbl).addClass('pure-table');
+        
+        for(var i = 0; i < 3; i++){
+            var tr = tbl.insertRow();
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(concepts[i].text));
+            
+            var td = tr.insertCell();
+            var relevance = '' + (concepts[i].relevance * 100 | 0) + '%';
+            var width = '' + ((concepts[i].relevance) * 100 | 0) - 8 + '%';
+            $(td).html('<div style = width:' + width + ';>' + relevance + '</div>');
+        }
+
+        $(tbl).prepend('<thead> <tr> <th>Concept</th> <th>Relevance</th> </tr> </thead>');
+        
+        $('#mainPopup').html("<h1>Key Concepts</h1>");
+        $('#mainPopup').append(tbl);
+
+
+        tbl  = document.createElement('table');
+        $(tbl).addClass('pure-table');
+        
+        for(var i = 0; i < 3; i++){
+            var tr = tbl.insertRow();
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(entities[i].text));
+            
+            var td = tr.insertCell();
+
+            var relevance = '' + (entities[i].relevance * 100 | 0) + '%';
+            var width = '' + ((entities[i].relevance) * 100 | 0) - 8 + '%';
+            $(td).html('<div style = width:' + width + ';>' + relevance + '</div>');
+                        
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(entities[i].type));
+            
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(entities[i].sentiment.type));
+                        
+        }
+
+        $(tbl).prepend('<thead> <tr> <th>Entity</th> <th>Relevance</th> <th>Type</th> <th>sentiment</th> </tr> </thead>');
+        
+        $('#mainPopup').append("<h1>Important Entities</h1>");
+        $('#mainPopup').append(tbl);
+
+    });
+    
+});
